50b87945a24100c1124ae2a27a824a82797c7641
0AYU0 <31642680+0AYU0@users.noreply.github.com>
2018-02-19 10:58:27 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,12 @@
-/*
-Title: RGB Filter
-Imagined, Designed, and Programmed by: Alexander Yu
-Date: 2/19/2018
-Description: 
-This filter intially changes the color of a pixel in an image to red, green or blue, based on the color of the pixel twenty-five pixels across.
-There are 4 filters that can be added on top that change the color of red, green, blue, or alpha, based on the red value.
-
-The links to the images used below are here : 
-img - https://www.desktop-background.com/p/2015/06/07/960475_vivi-ornitier-final-fantasy-ix-wallpapers-walldevil-best-free_1920x1080_h.jpg
-img1 - https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/vernal-equinox-march-20-first-day-spring-84@2x.jpg
-img2 - https://a04.t26.net/taringa/avatares/8/C/4/4/9/F/Lita_C/20D.jpg
-
-Includes Code From:
-* Lizzy Brooks
-* Danny Santana
-*/
-
 var img;
-var img1;
 var img2;
-var imgratio; //crops the image to a smaller size that fits the canvas
-var imgheight; //sets the new image height from the image ratio
+var img3;
+var img4;
+var img5;
+var button;
+var imgratio;
+var imgheight;
+var rSlider, gSlider, bSlider;
 
 //preload will load before setup runs
 function preload() {
@@ -28,17 +14,20 @@ function preload() {
   //In other words, in this example, make a folder called images in the same
   //folder as your sketch file (sketch.js) and then put the image file ("jomy.jpg")
   //inside of that.
+  //img = loadImage("sk8.jpg");
   img = loadImage("nLeNgYb.png");
-  //img1 = loadImage("vernal-equinox-march-20-first-day-spring-84@1x.jpg");
-  //img2 = loadImage("bright_colorful_wallpapers_002.jpg");
+  img2 = loadImage("nLeNgYb.jpg");
+  img3 = loadImage("vernal-equinox-march-20-first-day-spring-84@1x.jpg");
+  img4 = loadImage("bright_colorful_wallpapers_002.jpg");
+  img5 = loadImage("b1.jpg");
+
 }
 
-class ButtonClass{ //creates a class that allows you create buttons based on the 4 parameters below
-  constructor(input, x, y, func){
-    this.button = createButton(input)
-    this.button.position(x, y);
-    this.button.style('width', '75px')
-    this.button.mousePressed(func)
+class SliderClass {
+  constructor(x,y,length,center){
+    this.slider = createSlider(0, length, center);
+    this.slider.position(x,y);
+    this.slider.style('width', '200px');
   }
 }
 
@@ -47,13 +36,13 @@ function setup() {
   var imgratio = img.height/img.width;
   var imgheight = 1000*imgratio;
   createCanvas(1000, 1000*imgratio);
+  //image(img2, 0, 0);
   image(img, 0, 0, 1000, 1000*imgratio); //draw the image to the canvas
   console.log("Image width: 1000" + " height: " + 1000*imgratio);
   console.log(imgratio);
-  f1button = new ButtonClass('Filter 1', 10, img.height-200, f1); //creates a button for filter 1
-  f2button = new ButtonClass('Filter 2', 300, img.height-200, f2); //creates a button for filter 2
-  f3button = new ButtonClass('Filter 3', 590, img.height-200, f3); //creates a button for filter 3
-  f4button = new ButtonClass('Filter 4', img.width-440, img.height-200, f4); //creates a button for filter 4
+  redSlider = new SliderClass(0*img.width/8, img.height,255,random(255));
+  blueSlider = new SliderClass(0*img.width/8, img.height+120,255,random(255));
+  greenSlider = new SliderClass(0*img.width/8, img.height+60,255,random(255));
   loadPixels();
  for(var i=4*1000*0;i<16*1000*1000*imgratio;i+=4) {
    if (pixels[i] < pixels[i+100] && pixels[i+1] < pixels[i+101] && pixels[i+2] < pixels[i+102]) {
@@ -111,14 +100,15 @@ function setup() {
 
  }
 updatePixels();
-//f1();
-//f2();
-//f3();
-//f4();
+f1();
+f2();
+f3();
+f4();
+redFilter(0, img.height);
 }
 
 function redFilter(y1, y2){
-  fill(255, 0, 0, 50);
+  fill(255, 0, 0, 20);
   noStroke();
   rect(0, y1, 1000, y2);
 }
@@ -135,7 +125,7 @@ function blueFilter(y1, y2){
   rect(0, y1, 1000, y2);
 }
 
-function filter1(){ //changes red based on red
+function f1(){
   loadPixels();
  for (var i = 0; i < pixels.length; i+=4) {
    if (pixels[i] > 191) {
@@ -151,9 +141,9 @@ function filter1(){ //changes red based on red
  updatePixels();
 }
 
-function filter2(){ //changes green based on red 
+function f2(){
   loadPixels();
-  for (var i = 0; i < pixels.length; i+=4) { 
+  for (var i = 0; i < pixels.length; i+=4) {
     if (pixels[i] > 191) {
       pixels[i+1] = 255;
     } else if (pixels[i] > 127) {
@@ -167,7 +157,7 @@ function filter2(){ //changes green based on red
   updatePixels();
 }
 
-function filter3(){ //changes blue based on red
+function f3(){
   loadPixels();
   for (var i = 0; i < pixels.length; i+=4) {
     if (pixels[i] > 191) {
@@ -175,7 +165,7 @@ function filter3(){ //changes blue based on red
     } else if (pixels[i] > 127) {
       pixels[i+2] = 191;
     } else if (pixels[i] > 20) {
-      pixels[i+2] = 127;
+      pixels[i+2] = 0;
       } else {
       pixels[i+2] = 63;
     };
@@ -183,7 +173,7 @@ function filter3(){ //changes blue based on red
   updatePixels();
 }
 
-function filter4(){ //changes alpha based on red 
+function f4(){
   loadPixels();
   for (var i = 0; i < pixels.length; i+=4) {
     if (pixels[i] > 191) {
@@ -192,12 +182,24 @@ function filter4(){ //changes alpha based on red
       pixels[i+3] = 191;
     } else if (pixels[i] > 20) {
       pixels[i+3] = 127;
-    } else {
+      } else {
       pixels[i+3] = 63;
     };
   }
   updatePixels();
 }
 
+function changeBG() {
+  ellipse(random(0, 100), random(0, 100), 50, 50)
+}
+
 function draw() {
+  /*var redVal = redSlider.slider.value();
+  var greenVal = greenSlider.slider.value();
+  var blueVal = blueSlider.slider.value();
+  fill(redVal, greenVal, blueVal, 1);
+  noStroke();
+  rect(0, 0, img.width, img.height);
+  fill(255, 255, 255);*/
+
 }
